44d0157a9374857a5e1655002d9aa36821357c13
JBurrellIRL <jburrellirl@gmail.com>
2022-04-24 14:13:24 +0000

    Add function to welcome the player to the game

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
+"""
+Import random library and content in other
+Python files, for game operation.
+"""
+
 import random
+
 from wordlist import words
 from hangman_graphics import hangman_figures
 
@@ -13,7 +19,7 @@ print("""
       """)
 
 print(
-    "Welcome to my Hangman game! Guess the letters to uncover the random word.\n"
+    "Welcome to my Hangman game! Guess letters to discover the random word\n"
 )
 
 print("""Rules:
@@ -23,3 +29,24 @@ print("""Rules:
 -If you make 6 wrong guesses, the game ends.
 -Good luck!
     """)
+
+def welcome_user():
+    """
+    This function allows the player to input their name
+    before the game begins. The name field only
+    accepts letters.
+    """
+    username = None
+    while True:
+        username = input('Please enter your name:\n')
+
+        if not username.isalpha():
+            print('Name must include only letters.')
+            continue
+        else:
+            print(f'Welcome, {username}!')
+            print('================================')
+
+            break
+
+welcome_user()
